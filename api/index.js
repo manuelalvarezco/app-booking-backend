eb38bf1e2cbf46fc839f5ab7b181a02a851ee105
Manuel Alvarez <manuel.alvarez@pegasi.io>
2024-08-29 12:16:18 -0500

    update versel files

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const routerApi = require('./routes');
 const { logErrors, hendleError, boomErrorHandler } = require('./middlewares/handleError');
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('This is a REST API for app-booking-app');
 });
 
